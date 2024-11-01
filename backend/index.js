075372af0a35970ac27a3d0ca9429d2411a14503
Anil <anil6996f@gmail.com>
2024-11-01 11:40:44 +0530

    connected to BD

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    console.log('Connected to DB');
+}).catch((error) => {
+    console.log('error connecting to DB', error);
+});
+
 const app = express();
 
 
 app.listen(3000, () => {
     console.log('server running on port 3000');
 })
+
