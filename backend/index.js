a6040aa5de64473210e92ee944b865780e75f5ae
Anil <anil6996f@gmail.com>
2024-11-01 12:50:03 +0530

    added signup controller

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI).then(() => {
@@ -10,7 +13,10 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
 });
 
 const app = express();
+app.use(express.json());
 
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(3000, () => {
     console.log('server running on port 3000');
