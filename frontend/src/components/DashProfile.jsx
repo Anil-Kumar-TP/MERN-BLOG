98387d0ab42169b3aae193f7c9927c5be9e147a2
Anil <anil6996f@gmail.com>
2024-11-07 21:22:02 +0530

    added update user api

--- frontend/src/components/DashProfile.jsx
+++ frontend/src/components/DashProfile.jsx
@@ -1,17 +1,43 @@
 import { Button, TextInput } from 'flowbite-react';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useRef } from 'react';
 import {useSelector} from 'react-redux'
 
 export default function DashProfile () {
   
   const { currentUser } = useSelector((state) => state.user);
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const filePickerRef = useRef();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  }
+
+  // console.log(imageFile);
+  // console.log(imageFileUrl);
+
+  // useEffect(() => {
+  //   if (imageFile) {
+  //     uploadImage()
+  //   }
+  // }, [imageFile]);
+
+  // const uploadImage = () => {
+
+  // }
 
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
-          <img src={currentUser.profilePicture} alt="Profile Pic" className='w-full h-full rounded-full border-8 border-[lightgray] object-cover' />
+        <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden/>
+        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' onClick={()=>filePickerRef.current.click()}>
+          <img src={imageFileUrl || currentUser.profilePicture} alt="Profile Pic" className='w-full h-full rounded-full border-8 border-[lightgray] object-cover' />
         </div>
         <TextInput type='text' id='username' placeholder='Username' defaultValue={currentUser.username} />
         <TextInput type='email' id='email' placeholder='Email' defaultValue={currentUser.email} />
--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,12 +1,17 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-blog-2c0b7.firebaseapp.com",
-  projectId: "mern-blog-2c0b7",
-  storageBucket: "mern-blog-2c0b7.firebasestorage.app",
-  messagingSenderId: "515929183840",
-  appId: "1:515929183840:web:e17cc8a870df93b3add505"
+  apiKey: import.meta.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-estate-a23fb.firebaseapp.com",
+  projectId: "mern-estate-a23fb",
+  storageBucket: "mern-estate-a23fb.appspot.com",
+  messagingSenderId: "959764578389",
+  appId: "1:959764578389:web:1d000fe5278bd3acac94dc"
 };
 
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
